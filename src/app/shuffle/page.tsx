84a5dc69d607752d03d4b6c748902049feefba28
Tar <tar112@gmail.com>
2026-06-14 13:54:41 +0400

    feat: /shuffle page + panel

---
+++ src/app/shuffle/page.tsx
@@ -0,0 +1,7 @@
+import { ShufflePanel } from "@/components/shuffle/ShufflePanel";
+
+export const metadata = { title: "Shuffle" };
+
+export default function ShufflePage() {
+  return <ShufflePanel />;
+}
